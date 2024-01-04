68da00937698c8e6d015fa83a51f49a0045c920b
184anony <ochiken184@gmail.com>
2024-01-04 21:43:27 +0900

    add chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import './App.scss';
-import Sidebar from './components/sidebar/Sidebar';
+import React from "react";
+import "./App.scss";
+import Sidebar from "./components/sidebar/Sidebar";
+import Chat from "./components/chat/Chat";
 
 function App() {
-  return (
-    <div className="App">
-    {/* sidebar */}
-    <Sidebar />
+    return (
+        <div className="App">
+            {/* sidebar */}
+            <Sidebar />
 
-    {/* home */}
-    </div>
-  );
+            {/* chat */}
+            <Chat />
+        </div>
+    );
 }
 
 export default App;
